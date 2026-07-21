b09cfee5b7ecc9e6be4e807ff1ab4c02ea362f4e
Kareky <riccardo.morosin@hotmail.com>
2026-07-21 15:20:45 +0200

    feat(atkin): add Sieve of Atkin Algorithm

---
+++ sieves/atkin/atkin.go
@@ -0,0 +1,54 @@
+package atkin
+
+import "github.com/Kareky/primes/internal/errors"
+
+// FindPrimes returns all prime numbers up to upperBound using the Sieve of Atkin algorithm.
+// It returns an error if upperBound exceeds 1,000,000,000.
+func FindPrimes(upperBound int) ([]int, error) {
+	if upperBound > SizeLimit {
+		return nil, errors.ErrMaxSizeExceed(packageName, SizeLimit)
+	}
+
+	if upperBound < 2 {
+		return []int{}, nil
+	}
+
+	var primeList = []int{}
+	primeList = append(primeList, 2, 3)
+	// Slice stores odd numbers starting at 3: index i represents 2*i+3
+    // true = marked composite, false = still prime candidate
+	var numberList = make([]bool, upperBound+1)
+
+	for x := 1; x * x <= upperBound; x++ {
+		for y := 1; y * y <= upperBound; y++ {
+			n := (4 * x * x) + (y * y)
+			if n <= upperBound && (n % 12 == 1 || n % 12 == 5) {
+				numberList[n] = !numberList[n]
+			}
+
+			n = (3 * x * x) + (y * y)
+			if n <= upperBound && n % 12 == 7 {
+				numberList[n] = !numberList[n]
+			}
+
+			n = (3 * x * x) - (y * y)
+			if n <= upperBound && x > y && n % 12 == 11 {
+				numberList[n] = !numberList[n]
+			}
+		}
+	}
+
+	for n := 5; n <= upperBound; n+=2 {
+		if numberList[n] {
+			if n * n <= upperBound {
+				for i := n * n; i <= upperBound; i += n * n {
+					numberList[i] = false
+				}
+			}
+
+			primeList = append(primeList, n)
+		}
+	}
+
+	return primeList, nil
+}
---
+++ sieves/atkin/model.go
@@ -0,0 +1,4 @@
+package atkin
+
+const packageName = "atkin"
+const SizeLimit = 1000000000 //The limit to which number can be generated
